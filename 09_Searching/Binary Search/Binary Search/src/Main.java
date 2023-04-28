6e28aa42596c5e4af503b86ac0af1de0b6ed790f
Ayush sahu <74487708+mickygalaxy@users.noreply.github.com>
2023-04-29 01:27:15 +0530

    Add files via upload

---
+++ 09_Searching/Binary Search/Binary Search/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+abstract class BinarySerach{
+    interface Search{
+        int BSearch(int[] array, int value);
+    }
+}
+public class Main extends BinarySerach implements BinarySerach.Search {
+    @Override
+    public int BSearch(int[] array, int value) {
+        int left =0;
+        int right = array.length-1;
+
+        while(left<=right){
+            int mid = (left+(right-1))/2;
+            if(array[mid]==value){
+                return mid;
+            } else if (array[mid]<value) {
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:-");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        System.out.println("Enter the element:-");
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        System.out.println("Enter the Search value  :-");
+        int value = sc.nextInt();
+        Main Obj = new Main();
+        System.out.println("index ->" +Obj.BSearch(a, value));
+    }}
---
+++ 09_Searching/Linear Search/Linear Serach/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+abstract class LinearSerach{
+     interface Search{
+        void LSearch(int[] array, int value);
+    }
+        }
+
+public class Main extends LinearSerach implements LinearSerach.Search {
+    @Override
+    public void LSearch(int[] array, int value) {
+        for(int i =0;i<array.length;i++){
+            if(array[i]==value){
+                System.out.println("index - "+i+" ");
+                return;
+            }
+        }
+        System.out.println(-1+" ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:-");
+        int n = sc.nextInt();
+        int[]a = new int[n];
+        System.out.println("Enter the element:-");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println("Enter the Search value  :-");
+        int value=sc.nextInt();
+        Main Obj = new Main();
+        Obj.LSearch(a,value);
+
+    }
+}
