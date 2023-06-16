05f652424d517d9d96f332fee7c2cb6f5080b22b
Ayush sahu <74487708+mickygalaxy@users.noreply.github.com>
2023-06-16 13:59:50 +0530

    Add files via upload

---
+++ 14_Queue/Implementation_of_Queue_By_Array_LinkedList_Stack/03_DoubleEnded Queue(Deque)/ByLinkedList/By CollectiionFramework/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<Integer> dq = new LinkedList<>();
+        System.out.println(dq.isEmpty());
+        dq.addFirst(1);
+        dq.addFirst(2);
+        dq.addLast(3);
+        dq.addLast(4);
+        System.out.println(dq);
+        System.out.println(dq.peekFirst());
+        System.out.println(dq.getFirst());
+        System.out.println(dq.getLast());
+        System.out.println(dq.peekLast());
+        dq.removeFirst();
+        System.out.println(dq);
+        dq.removeLast();
+        System.out.println(dq);
+        System.out.println(dq.isEmpty());
+    }
+}
