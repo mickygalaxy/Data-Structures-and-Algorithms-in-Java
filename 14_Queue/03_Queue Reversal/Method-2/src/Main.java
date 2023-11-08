925d3ef2d39af7c6eb29ec2b2d31933cd0ca4ad5
Ayush sahu <74487708+mickygalaxy@users.noreply.github.com>
2023-11-08 23:52:02 +0530

    Update Main.java

--- 14_Queue/03_Queue Reversal/Method-2/src/Main.java
+++ 14_Queue/03_Queue Reversal/Method-2/src/Main.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner; 
 import java.util.Stack;
-
+import java.lang.*;
 public class Main {
     static Queue<Integer> q = new LinkedList<>();
 
