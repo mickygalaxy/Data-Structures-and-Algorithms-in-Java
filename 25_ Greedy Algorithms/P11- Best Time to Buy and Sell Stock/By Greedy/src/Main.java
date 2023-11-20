edc9fdd1f7d89a5434dfa61c9276e1282d76bfb6
Ayush sahu <74487708+mickygalaxy@users.noreply.github.com>
2023-11-20 23:51:39 +0530

    Update Main.java

--- 25_ Greedy Algorithms/P11- Best Time to Buy and Sell Stock/By Greedy/src/Main.java
+++ 25_ Greedy Algorithms/P11- Best Time to Buy and Sell Stock/By Greedy/src/Main.java
@@ -25,7 +25,7 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         int size = sc.nextInt();
         int[] prices = new int[size];
         for(int i=0;i<size;i++){
